5a43ebc5bb2ed43f3dc0a5c23853bba25a36586f
dgf0007 <dgf0007@alu.medac.es>
2023-03-27 22:01:04 +0200

    Proyecto Modificado-Factorial

--- com.entornos.Garcia_Fernandez_Daniel.algoritmos/src/com/entornos/Garcia_Fernandez_Daniel/algoritmos/algoritmos.java
+++ com.entornos.Garcia_Fernandez_Daniel.algoritmos/src/com/entornos/Garcia_Fernandez_Daniel/algoritmos/algoritmos.java
@@ -6,13 +6,15 @@ public abstract class algoritmos {
 	 * Método de fibonacci
 	 * Recibe un número por teclado y calcula el fibonacci de dicho número
 	 * 
-	 * @param es el número a calcular el fibonacci
+	 * @param Es el número a calcular el fibonacci
 	 *  
 	 * @version 1.0
 	 * 
-	 * @author Daniel
+	 * @author danie
+	 * 
+	 * @return devuelve el Fibonacci de dicho numero.
 	 *
-	 */
+	 **/
 	
 	public static int fibonacci(long numero) {
 		int x = 1, y = 0, z = 0;
@@ -24,5 +26,28 @@ public abstract class algoritmos {
 		}
 		return y;
 	}
+	
+	/**
+	 * Métodod para calcular el factorial de un número
+	 * Recibe un numero por teclado y calcula el factorial de dicho número
+	 * 
+	 * @param Es el número con el que se calculará el factorial
+	 * 
+	 * @version 1.0
+	 * 
+	 * @author danie
+	 * 
+	 * @return devuelve el factorial de dicho numero.
+	 **/
+	
+	
+	public static double factorial(long numero) {
+		double total = 1;
+		for (int a = 1; a <= numero; a++) {
+			total = total * a;
+		}
+		return total;
+	}
+
 
 }
