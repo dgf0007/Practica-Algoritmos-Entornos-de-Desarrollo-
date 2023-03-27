7051edd30881a011c6f60853b0281a56b8ca4532
dgf0007 <dgf0007@alu.medac.es>
2023-03-27 21:23:06 +0200

    Proyecto Modificado-Fibonacci

--- com.entornos.Garcia_Fernandez_Daniel.algoritmos/src/com/entornos/Garcia_Fernandez_Daniel/algoritmos/algoritmos.java
+++ com.entornos.Garcia_Fernandez_Daniel.algoritmos/src/com/entornos/Garcia_Fernandez_Daniel/algoritmos/algoritmos.java
@@ -1,5 +1,28 @@
 package com.entornos.Garcia_Fernandez_Daniel.algoritmos;
 
 public abstract class algoritmos {
+	
+	/**
+	 * Método de fibonacci
+	 * Recibe un número por teclado y calcula el fibonacci de dicho número
+	 * 
+	 * @param es el número a calcular el fibonacci
+	 *  
+	 * @version 1.0
+	 * 
+	 * @author Daniel
+	 *
+	 */
+	
+	public static int fibonacci(long numero) {
+		int x = 1, y = 0, z = 0;
+		for (int i = 1; i <= numero; i++) {
+			System.out.print(y + ", ");
+			z = y;
+			y = x;
+			x = x + z;
+		}
+		return y;
+	}
 
 }
